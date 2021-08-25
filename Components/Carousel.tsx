ced89a03345868f1bb16257f70494bc9682c481c
Sardar1208 <sarthak.bakre@gmail.com>
2021-08-25 16:50:57 +0530

    basic animation added

--- Components/Carousel.tsx
+++ Components/Carousel.tsx
@@ -1,37 +1,116 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Animated, {
+  useSharedValue,
+  interpolate,
+  useAnimatedStyle,
+  Extrapolate,
+} from "react-native-reanimated";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  StyleSheet,
+  StatusBar,
+} from "react-native";
 import { Box, Text, Image, HStack, ScrollView } from "native-base";
 
-export default function Carousel() {
+// since flatlist is not directly available in reanimated,
+// it is converted into custom component using this hook.
+const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
+
+const SIDECARD_LENGTH = 15;
+const SPACING = 5;
+const CARD_LENGTH = 300;
+
+const DATA = [
+  {
+    id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
+    title: "First Item",
+  },
+  {
+    id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
+    title: "Second Item",
+  },
+  {
+    id: "58694a0f-3da1-471f-bd96-145571e29d72",
+    title: "Third Item",
+  },
+];
+
+// the Item inside the flatlist (component)
+interface itemProps {
+  scroll: any;
+  index: number;
+}
+
+function Item({ scroll, index }: itemProps) {
+  // Stores the shared value opacity.
+  const opacity = useSharedValue(1);
+
+  const inputRange = [
+    (index - 1) * CARD_LENGTH,
+    index * CARD_LENGTH,
+    (index + 1) * CARD_LENGTH,
+  ];
+  opacity.value = interpolate(
+    scroll,
+    inputRange,
+    [0.5, 1, 0.5],
+    Extrapolate.CLAMP
+  );
+
+  useEffect(() => {
+    console.log("scale: ", scroll);
+  }, [scroll]);
+  const cardStyle = useAnimatedStyle(() => {
+    return {
+      // transform: [{ scaleY: scale.value }],
+      opacity: opacity.value,
+    };
+  });
+
   return (
-    <Box>
-      <ScrollView horizontal={true}>
-        <HStack space={5}>
-          <Box height={"56"} width={"72"} overflow={"hidden"}>
-            <Image
-              source={require("../assets/images/temp.jpg")}
-              alt={"product banner"}
-              size={"full"}
-              rounded={16}
-            />
-          </Box>
-          <Box height={"56"} width={"72"} overflow={"hidden"}>
-            <Image
-              source={require("../assets/images/temp.jpg")}
-              alt={"product banner"}
-              size={"full"}
-              rounded={16}
-            />
-          </Box>
-          <Box height={"56"} width={"72"} overflow={"hidden"}>
-            <Image
-              source={require("../assets/images/temp.jpg")}
-              alt={"product banner"}
-              size={"full"}
-              rounded={16}
-            />
-          </Box>
-        </HStack>
-      </ScrollView>
-    </Box>
+    <Animated.View style={[styles.card, cardStyle]}>
+      <Image
+        source={require("../assets/images/temp.jpg")}
+        alt={"product banner"}
+        size={"full"}
+        rounded={16}
+      />
+    </Animated.View>
   );
 }
+
+export default function Carousel() {
+  const [scrollX, setScrollX] = useState(0);
+
+  return (
+    <Animated.View>
+      <AnimatedFlatList
+        scrollEventThrottle={16}
+        horizontal={true}
+        data={DATA}
+        renderItem={({ item, index }) => {
+          console.log("index: ", index);
+          // Item is rendered having props - (scroll.x and index)
+          return <Item scroll={scrollX} index={index} />;
+        }}
+        //@ts-ignore
+        keyExtractor={(item) => item.id}
+        onScroll={(event) => {
+          // the scroll.x value is updated whenever list is scrolled.
+          setScrollX(event.nativeEvent.contentOffset.x);
+          console.log(scrollX);
+        }}
+      />
+    </Animated.View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    height: 200,
+    width: CARD_LENGTH,
+    overflow: "hidden",
+  },
+});
